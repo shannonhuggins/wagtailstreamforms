721f4dbe1e1b7d62353f481a65c56a6344e03362
Shannon <shannon@techequipt.com.au>
2025-06-12 16:04:52 +1000

    reverted wagtail-chooser changes

--- wagtailstreamforms/wagtail_hooks.py
+++ wagtailstreamforms/wagtail_hooks.py
@@ -4,8 +4,8 @@ from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.urls import include, path, reverse
 from django.utils.translation import gettext_lazy as _
-from wagtail.admin.viewsets.chooser import ModelChooserViewSet
-from wagtail.admin.widgets import AdminChooser
+from generic_chooser.views import ModelChooserViewSet
+from generic_chooser.widgets import AdminChooser
 from wagtail import hooks
 from wagtail.admin import messages as wagtail_messages
 from wagtail_modeladmin.helpers import AdminURLHelper, ButtonHelper
@@ -231,3 +231,24 @@ def process_form(page, request, *args, **kwargs):
                 )
 
 
+class WagtailStreamFormsChooserViewSet(ModelChooserViewSet):
+    icon = "form"
+    model = Form
+    page_title = _("Choose a form")
+    per_page = 10
+
+
+class WagtailStreamFormsChooser(AdminChooser):
+    choose_one_text = _("Choose a form")
+    choose_another_text = _("Choose another form")
+    link_to_chosen_text = _("Edit this form")
+    model = Form
+    choose_modal_url_name = "wagtailstreamforms_chooser:choose"
+    icon = "form"
+
+
+@hooks.register("register_admin_viewset")
+def register_wagtailstreamforms_chooser_viewset():
+    return WagtailStreamFormsChooserViewSet(
+        "wagtailstreamforms_chooser", url_prefix="wagtailstreamforms-chooser"
+    )
